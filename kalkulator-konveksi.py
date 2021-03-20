158f47a45798afb3ac135c0c6942ecee31ca45c3
Kevin Christian Chandra <kevinc.27999@gmail.com>
2021-03-20 17:49:11 +0700

    Created basic kalkulator-konveksi.

---
+++ kalkulator-konveksi.py
@@ -0,0 +1,94 @@
+class JobEntry:
+    def __init__(self, lusin, sisa, nominal):
+        self.lusin = lusin
+        self.sisa = sisa
+        self.nominal = nominal
+        self.subtotal = self.count_subtotal()
+    
+    def count_subtotal(self):
+        float_lusin = self.lusin + (self.sisa / 12)
+        return float_lusin*self.nominal
+    
+    def __str__(self):
+        return "{:5d} | {:4d} | {:>7s} | {:>10s}".format(self.lusin, self.sisa, format_currency(self.nominal), format_currency(self.subtotal))
+
+def format_currency(amount):
+    amount = str(int(amount))
+    formatted = ""
+    for i in range(len(amount)):
+        if (i % 3 == 0):
+            formatted = "," + formatted
+        formatted = amount[len(amount)-i-1] + formatted
+    return formatted[:-1]
+
+def count_total(lst):
+    print("Lusin | Sisa |  Biaya  |  Subtotal")
+    print("------------------------------------")
+    total = 0
+    for job in lst:
+        print(job)
+        total += job.subtotal
+    print("------------------------------------ +")
+    print("                  Total: {:>10s}".format(format_currency(total)))
+
+def delete_last(lst, num):
+    if (len(lst) == 0):
+        print("Job List is empty!")
+        return
+    if (not num):
+        print("Undo zero is useless!")
+        return
+    for i in range(num):
+        job = lst[-1]
+        print("Removed {} {} {}.".format(job.lusin, job.sisa, job.nominal))
+        lst.remove(job)
+
+def main():
+    print("""=====================================
+** Welcome to Konveksi Calculator! **
+
+* Type 'help' and Enter to show available commands.
+* Type 'exit' or 'quit' and Enter to exit..
+=====================================""")
+
+    keep_asking = True
+    job_list = list()
+    worker_name = ""
+    while keep_asking:
+        row = input(">>> ")
+        row = row.split()
+        if (row[0] == 'exit' or row[0] == 'quit'):
+            job_list = list()
+            keep_asking = False
+        elif (row[0] == 'undo'):
+            try:
+                num = int(row[1])
+                delete_last(job_list, num)
+            except(IndexError):
+                print("Undo needs lst and num arguments!")
+        elif (row[0] == 'total'):
+            print("Nama: {}".format(worker_name))
+            count_total(job_list)
+        elif (row[0] == 'reset'):
+            job_list = list()
+            worker_name = ""
+            print("============RESET============")
+        elif (row[0] == 'name' or row[0] == 'nama'):
+            worker_name = " ".join(row[1:])
+        elif (len(row) == 3):
+            lusin = int(row[0])
+            sisa = int(row[1])
+            nominal = int(row[2])
+            row_entry = JobEntry(lusin, sisa, nominal)
+            job_list.append(row_entry)
+        elif (len(row) == 2):
+            lusin = int(row[0])
+            sisa = 0
+            nominal = int(row[1])
+            row_entry = JobEntry(lusin, sisa, nominal)
+            job_list.append(row_entry)
+        else:
+            print("Format masukan salah. Masukkan dengan format yang benar.")
+
+if __name__ == '__main__':
+    main()
