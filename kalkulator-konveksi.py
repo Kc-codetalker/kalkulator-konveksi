2d88709781f4e1b9768ace06546c0fd450bf3d7b
Kevin Christian Chandra <kevinc.27999@gmail.com>
2021-11-20 16:10:42 +0700

    Moved payroll calculator to a new function to make it more modular.

--- kalkulator-konveksi.py
+++ kalkulator-konveksi.py
@@ -4,11 +4,11 @@ class JobEntry:
         self.sisa = sisa
         self.nominal = nominal
         self.subtotal = self.count_subtotal()
-    
+
     def count_subtotal(self):
         float_lusin = self.lusin + (self.sisa / 12)
         return float_lusin*self.nominal
-    
+
     def __str__(self):
         return "{:5d} | {:4d} | {:>7s} | {:>10s}".format(self.lusin, self.sisa, format_currency(self.nominal), format_currency(self.subtotal))
 
@@ -50,11 +50,34 @@ def delete_last(lst, num):
 def main():
     print("""=====================================
 ** Welcome to Konveksi Calculator! **
+=====================================""")
+    mode = input("""Please enter number below for which mode do you want to use.
+    (1) Payroll calculator ("gajian")
+    (2) Dozen calculator
+    (0) Exit
+>>>> """)
+    try:
+        mode = int(mode)
+    except:
+        print("Mode input should be a number!")
+        return
+
+    if (mode == 0):
+        print("Exitting...")
+    elif (mode == 1):
+        print("""You chose Payroll calculator.""")
+        print("""=====================================
+** Welcome to Konveksi Payroll Calculator! **
 
 * Type 'help' and Enter to show available commands.
 * Type 'exit' or 'quit' and Enter to exit..
 =====================================""")
+        payroll_calc_runner()
+    elif (mode == 2):
+        print("Dozen calculator is still in development. Please choose other mode.")
 
+
+def payroll_calc_runner():
     keep_asking = True
     job_list = list()
     worker_name = ""
